0ad48b20c17b2be00eb8fa3541188afc51ec6ad8
standupright <55686549+standupright@users.noreply.github.com>
2021-06-03 17:23:10 +0500

    Добавляет компоненту главной страницы и карточки товары, работа с пропсами

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Main from '../main/main';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {numberPlaces,places} = props;
+  return <Main numberPlaces={numberPlaces} places={places}/>;
 }
 
 export default App;
+
+App.propTypes = {
+  numberPlaces: PropTypes.number.isRequired,
+  places: PropTypes.arrayOf (
+    PropTypes.shape ({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+    })).isRequired,
+};
