83ccdd7d5465c112766ca8a14ca5108b5396a5ab
standupright <55686549+standupright@users.noreply.github.com>
2021-06-23 11:41:57 +0500

    Добавлена страница формы

---
+++ project/src/mocks/reviews.js
@@ -0,0 +1,28 @@
+const reviews = [
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: '2019-05-08T14:13:56.569Z',
+    id: 1,
+    rating: 1,
+    user: {
+      avatarUrl: 'img/avatar-max.jpg',
+      id: 1,
+      isPro: false,
+      name: 'Max',
+    },
+  },
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Orenburg.',
+    date: '2019-08-08T14:13:56.569Z',
+    id: 2,
+    rating: 5,
+    user: {
+      avatarUrl: 'img/avatar-angelina.jpg',
+      id: 2,
+      isPro: false,
+      name: 'Agata',
+    },
+  },
+];
+
+export default reviews;
---
+++ project/src/prop-validation/reviews.prop.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const reviewsPropShape = PropTypes.shape ({
+  comment: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  user: PropTypes.shape ({
+    avatarUrl: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequiredm,
+    isPro: PropTypes.bool.isRequired,
+    name: PropTypes.string.isRequired,
+  }),
+});
+
+export default reviewsPropShape;
+
