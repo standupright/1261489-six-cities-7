58427c9ff517aa5dab274270a9d28c72b457d57c
standupright <standupright@mail.ru>
2021-06-30 20:43:46 +0500

    Разделение Reviews на компоненты

--- project/src/components/reviews/reviews.jsx
+++ project/src/components/reviews/reviews.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import reviewsPropShape from '../../prop-validation/reviews.prop';
-import {RATING_MAX} from '../../const';
+import ReviewsList from '../reviews-list/reviews-list';
 import Form from '../form/form';
 
 function Reviews (props) {
@@ -12,40 +12,7 @@ function Reviews (props) {
       <h2 className="reviews__title">
         Reviews &middot; <span className="reviews__amount">{reviews.length}</span>
       </h2>
-      <ul className="reviews__list">
-        {reviews.map ( (comment) => (
-          <li key={comment.id} className="reviews__item">
-            <div className="reviews__user user">
-              <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                <img
-                  className="reviews__avatar user__avatar"
-                  src={comment.user.avatarUrl}
-                  width="54"
-                  height="54"
-                  alt="Reviews avatar"
-                />
-              </div>
-              <span className="reviews__user-name">
-                {comment.user.name}
-              </span>
-            </div>
-            <div className="reviews__info">
-              <div className="reviews__rating rating">
-                <div className="reviews__stars rating__stars">
-                  <span style={{width: `${comment.rating / RATING_MAX}%`}} />
-                  <span className="visually-hidden">{comment.rating}</span>
-                </div>
-              </div>
-              <p className="reviews__text">
-                {comment.comment}
-              </p>
-              <time className="reviews__time" dateTime={comment.date}>
-                {comment.date}
-              </time>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <ReviewsList reviews={reviews} />
       <Form />
     </section>
   );
